570309ec0f0dea70f51b163cbcd893322c964e77
Lunarixus <Nathan@lunarixus.dev>
2021-07-16 15:41:02 +0530

    [SQUASH]miatoll: Remove packages at build time
    Squash of the following:
    
    miatoll: remove_packages: Rename RemovePackages for source
    
    miatoll: remove_packages: Switch to platform certificate
    
    Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: Arpon <arpon.sarkar5.6@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -312,6 +312,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
